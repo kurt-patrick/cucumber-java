23f59a9cb97485071c4fc1b44959691ba4bb2d0c
kp <kurt_patrick_online@hotmail.com>
2018-07-18 20:48:42 +0800

    added feature and steps file for login

---
+++ cucumber-maven/CucumberWithMaven/src/test/java/CucumberWithMaven/stepFiles/Login.java
@@ -0,0 +1,48 @@
+package CucumberWithMaven.stepFiles;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Login {
+
+	@Given("^user navigates to stackoverflow website$")
+	public void user_navigates_to_stackoverflow_website() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new PendingException();
+	}
+
+	@And("^user clicks on the login button on homepage$")
+	public void user_clicks_on_the_login_button_on_homepage() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@And("^user enters a valid username$")
+	public void user_enters_a_valid_username() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@And("^user enters a valid password$")
+	public void user_enters_a_valid_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user clicks on the login button$")
+	public void user_clicks_on_the_login_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user should be taken to the successful login page$")
+	public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
